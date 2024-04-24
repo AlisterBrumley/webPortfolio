a62b0800d21a7f821ad4cf4d195f5c55776b589a
Alister Brumley <109202180+AlisterBrumley@users.noreply.github.com>
2024-04-24 22:54:19 +1000

    added new icons, new light/dark setting in progress
    need to finish light/dark toggle, and create default

--- scripts.js
+++ scripts.js
@@ -1,3 +1,15 @@
 const images = document.getElementsByClassName("exampleImage")
 const summaries = document.getElementById("exampleSummary")
+const button = document.getElementById("modeButton")
 
+if (window.matchMedia) {
+    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        console.log("dark")
+    } else {
+        console.log("light")
+    }
+}
+
+// NEED TO ADD QUERY OF DATATHEME THEN SWITCH
+// will need to swap pictures somehow as well
+button.addEventListener("click", () => document.documentElement.setAttribute("data-theme", "light"))
