7c39d99eeb85a83be5c3edc73ef32875ed7e0dbc
Alister Brumley <109202180+AlisterBrumley@users.noreply.github.com>
2024-04-25 16:54:24 +1000

    got image src's
    will have to break them up for correct setting

--- scripts.js
+++ scripts.js
@@ -1,15 +1,38 @@
-const images = document.getElementsByClassName("exampleImage")
-const summaries = document.getElementById("exampleSummary")
-const button = document.getElementById("modeButton")
 
+const summaries = document.getElementById("exampleSummary")
+const modeButton = document.getElementById("modeButton")
+let darkMode = true
+
+// checks if light mode, otherwise default to dark
 if (window.matchMedia) {
-    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-        console.log("dark")
+    if (window.matchMedia('(prefers-color-scheme: light)').matches) {
+        document.documentElement.setAttribute("data-theme", "light")
+        darkMode = false
     } else {
-        console.log("light")
+        document.documentElement.setAttribute("data-theme", "dark")
+        darkMode = true
     }
+}
+
+
+function imageSrcChange() {
+    const images = document.getElementsByClassName("exampleImage")
+    Array.from(images).forEach(image => {
+        let oldSrc = image.src
+        console.log(oldSrc)
+    });
 }
 
 // NEED TO ADD QUERY OF DATATHEME THEN SWITCH
 // will need to swap pictures somehow as well
-button.addEventListener("click", () => document.documentElement.setAttribute("data-theme", "light"))
+modeButton.addEventListener("click", () => {
+    if (darkMode) {
+        document.documentElement.setAttribute("data-theme", "light")
+        darkMode = false
+    } else {
+        document.documentElement.setAttribute("data-theme", "dark")
+        darkMode = true
+    }
+})
+
+imageSrcChange()
